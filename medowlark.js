d14717f2d02d4a141637a13e82efcb0a175fb9af
changshawulin <392457651@qq.com>
2018-02-27 11:38:02 +0800

    add tour pages

--- medowlark.js
+++ medowlark.js
@@ -14,12 +14,25 @@ app.set("view engine", "handlebars");
 
 app.set("port", process.env.PORT || 8090);
 
+app.use(express.static(__dirname + "/public"));
+
+app.use((req, res, next) => {
+    res.locals.showTests = app.get("env") !== "production" && req.query.test === "1";
+    next();
+});
+
 app.get("/", (req, res) => res.render("home"));
 
+app.get("/about", (req, res) => {
+    res.render("about", {
+        fortune: fortune.getFortune(),
+        pageTestScript: "/qa/tests-about.js"
+    })
+});
 
-app.get("/about", (req, res) => res.render("about", {fortune: fortune.getFortune()}));
+app.get("/tours/hood-river", (req, res) => res.render("tours/hood-rivers"));
 
-app.use(express.static(__dirname + "/public"));
+app.get("/tours/request-group-rate", (req, res) => res.render("tours/request-group-rate"));
 
 //定制404页面
 app.use((req, res, next) => {
